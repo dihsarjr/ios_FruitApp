9c917062d415f90deb99ff60141a40668660d57e
rashid <mohammed1rashid786@gmail.com>
2021-07-14 13:59:22 +0530

    Initial Commit

---
+++ Fructus/FructusApp.swift
@@ -0,0 +1,17 @@
+//
+//  FructusApp.swift
+//  Fructus
+//
+//  Created by Rashid on 14/07/21.
+//
+
+import SwiftUI
+
+@main
+struct FructusApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
